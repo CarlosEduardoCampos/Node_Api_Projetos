3354f9ef7dc0615e04ab54ec098d3c2b71bb939d
Carlos <carlos.admcode@gmail.com>
2022-09-25 18:09:25 -0300

    Cria estrutura de pasta e cofigura a porta 3000 para o servidor node

---
+++ servidor_aula/src/app.js
@@ -0,0 +1,18 @@
+
+let express = require('express');
+let consign = require('consign')
+
+let app = express()
+
+/* Configura middlewares*/
+app.use(express.json())
+
+/* Faz o autoload das rotas, dos models e do controllers para o objeto app */
+consign()
+    .include('src/routes')
+    .then('src/models')
+    .then('src/controllers')
+.into(app);
+
+
+module.exports = app;
