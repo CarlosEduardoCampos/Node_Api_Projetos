576116a1971607efa6ad50abd9c5fb1a0ba5a2f3
Carlos <carlos.admcode@gmail.com>
2023-11-02 20:34:29 -0300

    add Dark tema e a mudança entre os temas

--- crud_site_typescript/youtube_curso/src/App.tsx
+++ crud_site_typescript/youtube_curso/src/App.tsx
@@ -3,18 +3,15 @@ import { BrowserRouter } from 'react-router-dom'; // Biblioteca de redirecioname
 import { AppRoutes } from './routes'; // Arquivo de configuração de rotas
 
 // Estilo da Aplicação:
-import { ThemeProvider } from '@mui/material'; // Ferramenta de cofiguração de estilo
-import { LightTheme } from './shared/themes'; // Cofiguração manual de estilo
+import { AppThemeProvider } from './shared/contexts/ThemeContext';
 
 export const App = ()=>{
   return (
     // Tag de estilo criado manualmente:
-    <ThemeProvider theme={LightTheme}>
-      
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-
-    </ThemeProvider>
+    <AppThemeProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+    </AppThemeProvider>
   );
 }
