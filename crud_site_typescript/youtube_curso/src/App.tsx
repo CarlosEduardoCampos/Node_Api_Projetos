c438c9bfde8fe444952cd82a3985185011e9f4bd
Carlos <carlos.admcode@gmail.com>
2023-11-02 19:47:16 -0300

    add mui e cria uma cofiguração de tema claro

--- crud_site_typescript/youtube_curso/src/App.tsx
+++ crud_site_typescript/youtube_curso/src/App.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
+// Funcionalidade da Aplicação:
+import { BrowserRouter } from 'react-router-dom'; // Biblioteca de redirecionamento de requisições
+import { AppRoutes } from './routes'; // Arquivo de configuração de rotas
+
+// Estilo da Aplicação:
+import { ThemeProvider } from '@mui/material'; // Ferramenta de cofiguração de estilo
+import { LightTheme } from './shared/themes'; // Cofiguração manual de estilo
 
 export const App = ()=>{
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    // Tag de estilo criado manualmente:
+    <ThemeProvider theme={LightTheme}>
+      
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+
+    </ThemeProvider>
   );
 }
--- crud_site_typescript/youtube_curso/src/routes/index.tsx
+++ crud_site_typescript/youtube_curso/src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@mui/material';
 import{ Routes, Route, Navigate} from 'react-router-dom';
 
 export const  AppRoutes = () =>{
     return(
         <Routes>
-            <Route path="/page-inicial" element={<p>Página Inicial</p>} />
-            <Route path="*" element={<Navigate to="/page-inicial"/>} />
+            <Route path='/page-inicial' element={<Button variant='contained' color='primary'>Teste</Button>} />
+            <Route path='*' element={<Navigate to='/page-inicial'/>} />
         </Routes>
     );
 }
